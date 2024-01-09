7d28225adaad0d46b85a891979b8edbe19ee1f2d
Sridevi P <112706237+sridevi0323@users.noreply.github.com>
2024-01-09 14:18:16 +0530

    Delete Append.java

--- Append.java
+++
@@ -1,9 +0,0 @@
-public class Append {
-    public static void main(String[] args) {
-        String s ="I am a good boy";
-        char charToAppend='!';
-        StringBuilder append=new StringBuilder(s);
-        append.append(charToAppend);
-        System.out.println("Appended String: "+append.toString());
-    }
-}
