2bb62a59bebd8cb22837772e444bdae3f2c1356e
Sridevi P <112706237+sridevi0323@users.noreply.github.com>
2024-01-09 14:22:33 +0530

    Delete Substing.java

--- Substing.java
+++
@@ -1,8 +0,0 @@
-public class Substing {
-        public static void main(String[] args) {
-            String s = "I'm a good boy";
-            String substring = s.substring(6, 12);
-            System.out.println("Substring from index 6 to 10: " + substring);
-        }
-    }
-
